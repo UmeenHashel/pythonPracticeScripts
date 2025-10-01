86cfdc83213c2b7d94a38e8a6185ca574b3ccc63
UmeenHashel <umeenbg16@gmail.com>
2025-10-02 04:24:18 +0530

    Commit

--- ex5.py
+++ ex5.py
@@ -1,5 +1,20 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+import random
+
+a = []
+b = []
+
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+for i in range(10):
+    a.append(random.randint(1, 10))
+
+for i in range(15):
+    b.append(random.randint(1, 15))
+
+print(a)
+print(b)
+
 n = []
 
 for element in a:
